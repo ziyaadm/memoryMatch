767f44b4cfc3cb749addabf091dfbadd21c0fc84
Ziyaad <ziyaadm@gmail.com>
2019-10-02 10:12:10 -0700

    added flip one card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+$(document).ready(initializeApp);
 
+function initializeApp(){
+  $(".cards").find(".back").on("click", handleCardClick);
+}
+
+function handleCardClick(event){
+  $(event.currentTarget).addClass("hidden");
+  console.log(event);
+}
