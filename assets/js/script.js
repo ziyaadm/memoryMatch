c1c21aac55d79a0ac7dec28cae5c8ef2659ec947
Ziyaad <ziyaadm@gmail.com>
2019-10-02 18:44:48 -0700

    added match two card feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,44 @@
 $(document).ready(initializeApp);
 
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+var firstCardBack = null;
+var secondCardBack = null;
+
 function initializeApp(){
-  $(".cards").find(".back").on("click", handleCardClick);
+  $(".cards").on("click", handleCardClick);
 }
 
 function handleCardClick(event){
-  $(event.currentTarget).addClass("hidden");
-  console.log(event);
+
+  $(event.currentTarget).find(".back").addClass("hidden");
+
+  if (firstCardClicked === null){
+
+    firstCardClicked = $(event.currentTarget).find(".front").css("background-image");
+    firstCardBack = $(event.currentTarget).find(".back");
+
+     console.log("first card clicked",firstCardClicked);
+  }else {
+    secondCardClicked = $(event.currentTarget).find(".front").css("background-image");
+    secondCardBack = $(event.currentTarget).find(".back");
+    console.log("second card clicked",secondCardClicked);
+  }
+
+if(firstCardClicked === secondCardClicked){
+  console.log("card match");
+  matches++;
+} else if (firstCardClicked === null || secondCardClicked === null) {
+  return;
+}
+ else if (firstCardClicked !== secondCardClicked){
+  setTimeout(function(){
+    $(firstCardBack).removeClass("hidden");
+    $(secondCardBack).removeClass("hidden");
+  }, 1500);
+  firstCardClicked = null;
+  secondCardClicked = null;
+}
+
 }
