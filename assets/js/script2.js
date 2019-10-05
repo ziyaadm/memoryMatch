5e955047b6e928a76bf1bce967a578bc1c30565b
Ziyaad <ziyaadm@gmail.com>
2019-10-05 10:50:01 -0700

    rough draft

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -11,50 +11,46 @@ var attempts = 0;
 var games_played = 0;
 var accuracy = 0;
 
-function initializeApp(){
+function initializeApp() {
   modal = $(".modal")
   $(".cards").on("click", handleCardClick);
 }
-function calculateAccuracy(){
-accuracy = Math.floor((matches / attempts) * 100);
-console.log(accuracy);
-displayStats();
-//return accuracy;
-}
-function displayStats(){
-$(".playedNum").text(games_played);
-$(".attemptNum").text(attempts);
-$(".accuracyNum").text(accuracy);
-}
-function resetStats(){
-  matches = null;
-  attempts = null;
-  accuracy = null;
-  games_played++;
+
+function calculateAccuracy() {
+  accuracy = Math.floor((matches / attempts) * 100);
+  console.log(accuracy);
   displayStats();
-  $(modal).addClass("hidden");
-  $(".back").removeClass("hidden");
+  //return accuracy;
 }
-function handleCardClick(event){
+
+function displayStats() {
+  $(".playedNum").text(games_played);
+  $(".attemptNum").text(attempts);
+  $(".accuracyNum").text(accuracy + "%");
+}
+
+function handleCardClick(event) {
   displayStats();
   $(event.currentTarget).find(".back").addClass("hidden");
 
-  if (firstCardClicked === null){
+  if (firstCardClicked === null) {
+
     firstCardClicked = $(event.currentTarget).find(".front").css("background-image");
     firstCardBack = $(event.currentTarget).find(".back");
-    console.log("first card clicked",firstCardClicked);
+    console.log("first card clicked", firstCardClicked);
     displayStats();
-  }else {
+
+  } else {
     secondCardClicked = $(event.currentTarget).find(".front").css("background-image");
     secondCardBack = $(event.currentTarget).find(".back");
-    console.log("second card clicked",secondCardClicked);
+    console.log("second card clicked", secondCardClicked);
     attempts++;
     console.log(attempts);
     calculateAccuracy();
     $(".cards").off("click");
   }
 
-  if(firstCardClicked === secondCardClicked){
+  if (firstCardClicked === secondCardClicked) {
     console.log("card match");
     firstCardClicked = null;
     secondCardClicked = null;
@@ -64,22 +60,19 @@ function handleCardClick(event){
     if (matches === max_matches) {
       games_played++
       displayStats();
+      $(".cards").on("click", handleCardClick);
       $(modal).removeClass("hidden");
-      $("#resetButton").on("click", resetStats);
-      }
-}  else if (firstCardClicked === null || secondCardClicked === null) {
+    }
+  } else if (firstCardClicked === null || secondCardClicked === null) {
     return;
-}  else if (firstCardClicked !== secondCardClicked){
+  } else if (firstCardClicked !== secondCardClicked) {
     calculateAccuracy();
-    setTimeout(function(){
+    setTimeout(function () {
       $(firstCardBack).removeClass("hidden");
       $(secondCardBack).removeClass("hidden");
       $(".cards").on("click", handleCardClick);
     }, 1500);
     firstCardClicked = null;
     secondCardClicked = null;
-    }
-    else if (firstCardClicked === firstCardClicked){
-      return;
-    }
   }
+}
